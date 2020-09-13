2f36f627685b61d845f2e8500bcb7745c8f07ee2
JesseLopesTI <jesselopes.ti2020@gmail.com>
2020-09-13 17:11:56 -0300

    Definindo o algoritmo do script

---
+++ tabuada-de-soma/script/main.js
@@ -0,0 +1,24 @@
+var numero;
+
+do {
+  numero = parseFloat(prompt("Digite um número entre 0 e 10: "));
+
+  if (numero <= 10) {
+    console.log("A tabuada no número " + numero + " é:");
+  } else {
+    while (numero > 10) {
+      console.log("Observação:");
+      console.log("Só é permitido um número entre 0 e 10. Tente novamente!")
+
+      numero = parseFloat(prompt("Digite um número entre 0 e 10: "));
+
+      numero = numero + 0;
+    }
+
+    console.log("A tabuada do número " + numero + " é:");
+  }
+
+  for (var contador = 0; contador <= 10; contador++) {
+    console.log(numero + " + " + contador + " = " + (numero + contador));
+  }
+} while (numero <= 10);
