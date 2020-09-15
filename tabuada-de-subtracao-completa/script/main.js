6ce85386f736f36992b95741c3779ae53fd9395e
JesseLopesTI <jesselopes.ti2020@gmail.com>
2020-09-15 11:01:33 -0300

    Definindo o algoritmo do script

---
+++ tabuada-de-subtracao-completa/script/main.js
@@ -0,0 +1,6 @@
+for (var unidade = 0; unidade <= 10; unidade++) {
+  console.log("A tabuada do número " + unidade + " é:");
+  for (var dezena = 0; dezena <= 10; dezena++) {
+    console.log(unidade + " - " + dezena + " = " + (unidade - dezena));
+  }
+}
