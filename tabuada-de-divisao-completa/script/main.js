2be9fd293ef20c44f49411238541876a891a7c15
JesseLopesTI <jesselopes.ti2020@gmail.com>
2020-09-15 11:19:02 -0300

    Definindo o algoritmo do script

---
+++ tabuada-de-divisao-completa/script/main.js
@@ -0,0 +1,70 @@
+var numero1 = 1;
+var numero2 = 2;
+var numero3 = 3;
+var numero4 = 4;
+var numero5 = 5;
+var numero6 = 6;
+var numero7 = 7;
+var numero8 = 8;
+var numero9 = 9;
+var numero10 = 10;
+
+console.log("A tabuada do número " + numero1 + " é:");
+
+for (var dezena = 0; dezena <= 10; dezena++) {
+  console.log(dezena + " / " + numero1 + " = " + dezena / numero1);
+}
+
+console.log("A tabuada do número " + numero2 + " é:");
+
+for (var dezena = 0; dezena <= 20; dezena = dezena + 2) {
+  console.log(dezena + " / " + numero2 + " = " + dezena / numero2);
+}
+
+console.log("A tabuada do número " + numero3 + " é:");
+
+for (var dezena = 0; dezena <= 30; dezena = dezena + 3) {
+  console.log(dezena + " / " + numero3 + " = " + dezena / numero3);
+}
+
+console.log("A tabuada do número " + numero4 + " é:");
+
+for (var dezena = 0; dezena <= 40; dezena = dezena + 4) {
+  console.log(dezena + " / " + numero4 + " = " + dezena / numero4);
+}
+
+console.log("A tabuada do número " + numero5 + " é:");
+
+for (var dezena = 0; dezena <= 50; dezena = dezena + 5) {
+  console.log(dezena + " / " + numero5 + " = " + dezena / numero5);
+}
+
+console.log("A tabuada do número " + numero6 + " é:");
+
+for (var dezena = 0; dezena <= 60; dezena = dezena + 6) {
+  console.log(dezena + " / " + numero6 + " = " + dezena / numero6);
+}
+
+console.log("A tabuada do número " + numero7 + " é:");
+
+for (var dezena = 0; dezena <= 70; dezena = dezena + 7) {
+  console.log(dezena + " / " + numero7 + " = " + dezena / numero7);
+}
+
+console.log("A tabuada do número " + numero8 + " é:");
+
+for (var dezena = 0; dezena <= 80; dezena = dezena + 8) {
+  console.log(dezena + " / " + numero8 + " = " + dezena / numero8);
+}
+
+console.log("A tabuada do número " + numero9 + " é:");
+
+for (var dezena = 0; dezena <= 90; dezena = dezena + 9) {
+  console.log(dezena + " / " + numero9 + " = " + dezena / numero9);
+}
+
+console.log("A tabuada do número " + numero10 + " é:");
+
+for (var dezena = 0; dezena <= 100; dezena = dezena + 10) {
+  console.log(dezena + " / " + numero10 + " = " + dezena / numero10);
+}
